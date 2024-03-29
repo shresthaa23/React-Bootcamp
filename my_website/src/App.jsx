af24b06ce159358a5149e445c87d71856cae4f95
Alex Shrestha <ashrest6@terpmail.umd.edu>
2024-03-29 12:50:29 -0400

    added sections + contacts

--- my_website/src/App.jsx
+++ my_website/src/App.jsx
@@ -5,19 +5,37 @@ import './App.css'
 
 // http://localhost:5173/
 
+const contacts = [
+  { title: "Email", content: "ashrest6@terpmail.umd.edu", link: "mailto: ashrest6@terpmail.umd.edu" }, 
+  { title: "Linkedin", content: "Alex Shrestha", link: "https://www.linkedin.com/in/shrestha2023" },
+  { title: "Instagram", content: "@alex_shreds82", link: "https://www.instagram.com/alex_shreds82" },
+  { title: "GitHub", content: "@shresthaa23", link: "https://github.com/shresthaa23"},
+];
+
+
 function HomePage() {
   return (
-    <div class="header">
-    <div class="intro_text">        
-        <h1> Alex Shrestha </h1> 
-        <h2> Welcome to my Website </h2>
+    <div className="header">
+      <div className="intro_text">        
+          <h1> Alex Shrestha </h1> 
+          <h3> Welcome to my Website </h3>
+      </div>
     </div>
-  </div>
   );
 }
 
-function AboutMe() {
 
+function AboutMe() {
+  return (
+    <div className="aboutMe">     
+          <h2 className="title1"> About Me </h2> 
+          <p className="life">
+            Hey, I'm Alex. I'm currently an undergrad student @ <em>The University of Maryland College Park</em> studying <em>Computer Science</em>. 
+            I'm apart of the <em>ACES LLP</em> and an <em>App Dev</em> Bootcamp member!     
+            In my free time I like going to the <em>gym, playing games</em> and <em>watching shows</em>. I just finished Peaky Blinders and I'm currently watching Modern Family & Snowfall.
+          </p>
+    </div>
+  );
 }
 
 function Experience() {
@@ -33,6 +51,25 @@ function Technical() {
 }
 
 function ContactMe() {
+  return (
+    <div>
+      <h2> Contact Me </h2> 
+      <h3><a href="mailto: ashrest6@terpmail.umd.edu"> Email </a></h3>
+      <h3><a href="https://www.linkedin.com/in/shrestha2023"> Linkedin </a></h3>
+      <h3><a href="https://www.instagram.com/alex_shreds82"> Instagram </a></h3>
+      <h3><a href="https://github.com/shresthaa23"> GitHub </a></h3>
+    </div>
+
+    );
+}
+
+function MyContacts({ title, content, link }) {
+  return (
+    <div className="socials">
+      <h1>{title}</h1>
+      <a href={link}>{content}</a>
+    </div>
+  );
 
 }
 
@@ -73,8 +110,21 @@ function App() {
     <><>
       <NavBar />
     </><>
-        <HomePage />
-      </></>    
+        <HomePage />          
+      </>
+      <>
+        <AboutMe />
+        <div className="contactsss">
+            {contacts.map((element) => (
+              <MyContacts title={element.title} content={element.content} link={element.link} />
+            ))}
+          </div>
+          
+      </>
+   
+    </>
+
+
   )
 }
 
