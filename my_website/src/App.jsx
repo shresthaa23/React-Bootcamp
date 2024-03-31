3857c10fa137326c23760c01168678a9537d8cf4
Alex Shrestha <ashrest6@terpmail.umd.edu>
2024-03-31 12:58:15 -0400

    added sections and more stuff

--- my_website/src/App.jsx
+++ my_website/src/App.jsx
@@ -3,8 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// http://localhost:5173/
-
 const contacts = [
   { title: "Email", content: "ashrest6@terpmail.umd.edu", link: "mailto: ashrest6@terpmail.umd.edu" }, 
   { title: "Linkedin", content: "Alex Shrestha", link: "https://www.linkedin.com/in/shrestha2023" },
@@ -12,6 +10,37 @@ const contacts = [
   { title: "GitHub", content: "@shresthaa23", link: "https://github.com/shresthaa23"},
 ];
 
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <p href="/" className="myName">Alex Shrestha</p>
+      <ul>
+        <li>
+          <a className="nav" href="/"> Home </a>
+        </li>
+        <li>
+          <a className="nav" href="/"> About Me </a>
+        </li>
+        <li>
+          <a className="nav" href="/"> Experience </a>
+        </li>
+        <li>
+          <a className="nav" href="https://cdn.vox-cdn.com/thumbor/bz2Y1F1_NTbKLfnINojph4jGVzQ=/0x0:2880x1434/1200x800/filters:focal(1133x488:1593x948)/cdn.vox-cdn.com/uploads/chorus_image/image/71969421/Screen_Shot_2023_02_12_at_6.36.56_PM.0.png"> Batman </a>
+        </li>
+        <li>
+          <a className="nav" href="/"> Technical </a>
+        </li>
+        <li>
+          <a className="nav" href="https://i.kym-cdn.com/entries/icons/original/000/048/705/stilgar_dune.jpg"> Lisan Al Gaib </a>
+        </li>
+        <li>
+          <a className="nav" href="/"> Contact Me </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 function HomePage() {
   return (
@@ -28,82 +57,98 @@ function HomePage() {
 function AboutMe() {
   return (
     <div className="aboutMe">     
-          <h2 className="title1"> About Me </h2> 
-          <p className="life">
+          <h2 className="aboutMeTitle"> About Me </h2> 
+          <p className="aboutMeText">
             Hey, I'm Alex. I'm currently an undergrad student @ <em>The University of Maryland College Park</em> studying <em>Computer Science</em>. 
-            I'm apart of the <em>ACES LLP</em> and an <em>App Dev</em> Bootcamp member!     
+            I'm apart of the <em>ACES LLP</em> and an <em>App Dev</em> Bootcamp member!
             In my free time I like going to the <em>gym, playing games</em> and <em>watching shows</em>. I just finished Peaky Blinders and I'm currently watching Modern Family & Snowfall.
           </p>
+          <img class="myphoto" src="/images/me.jpg" alt="photo of me"></img>
     </div>
   );
-}
-
-function Experience() {
-
 }
 
 function Batman() {
-// Lisan Al Gaib
+  return (
+    <div className="batman">
+      <h7> I am Batman. </h7>
+    </div>
+  );
 }
 
 function Technical() {
-
-}
-
-function ContactMe() {
   return (
-    <div>
-      <h2> Contact Me </h2> 
-      <h3><a href="mailto: ashrest6@terpmail.umd.edu"> Email </a></h3>
-      <h3><a href="https://www.linkedin.com/in/shrestha2023"> Linkedin </a></h3>
-      <h3><a href="https://www.instagram.com/alex_shreds82"> Instagram </a></h3>
-      <h3><a href="https://github.com/shresthaa23"> GitHub </a></h3>
+    <div className="tech">
+      <div className="projects">
+      <h2 className="projectsTitle"> Projects </h2>
+        <div className="projectsText">
+          <li>
+            <a> This. </a>
+          </li>
+          <li>
+            <a> Document Manager in C </a>
+          </li>
+          <li>
+            <a> Maze Traversals in Java </a>
+          </li>
+          <li>
+            <a> Data Analysis in Python </a>
+          </li>
+        </div>
+        </div>
+      <div className="languages">
+      <h2 className="languagesTitle"> Languages </h2>
+        <li>
+            <a> Java </a>
+          </li>
+          <li>
+            <a> C </a>
+          </li>
+          <li>
+            <a> Python </a>
+          </li>
+          <li>
+            <a> HTML/CSS </a>
+          </li>
+          <li>
+            <a> JavaScript </a>
+          </li>
+          <li>
+            <a> React </a>
+          </li>
+          <li>
+            <a> Git </a>
+          </li>
+        <li className="joke">
+          <a> last few may be a joke </a>
+        </li>
+        </div>
     </div>
-
-    );
+  );
 }
 
+function Contact() {
+  return (
+    <div className="contacts">
+      <div className="contactsTitle">
+        <h1> Connect with me!</h1>
+      </div>
+      {contacts.map((element) => (
+        <MyContacts title={element.title} content={element.content} link={element.link} />
+      ))}
+    </div>
+  )
+}
 function MyContacts({ title, content, link }) {
   return (
-    <div className="socials">
-      <h1>{title}</h1>
-      <a href={link}>{content}</a>
+    <div>
+      <h3>{title}</h3>
+      <a className="socials" href={link}>{content}</a>
     </div>
   );
 
 }
 
-
-function NavBar() {
-  return (
-    <nav className="navbar">
-      <a href="/" className="myName">Alex Shrestha</a>
-      <ul>
-        <li>
-          <a href="/"> Home </a>
-        </li>
-        <li>
-          <a href="/"> About Me </a>
-        </li>
-        <li>
-          <a href="/"> Experience </a>
-        </li>
-        <li>
-          <a href="https://cdn.vox-cdn.com/thumbor/bz2Y1F1_NTbKLfnINojph4jGVzQ=/0x0:2880x1434/1200x800/filters:focal(1133x488:1593x948)/cdn.vox-cdn.com/uploads/chorus_image/image/71969421/Screen_Shot_2023_02_12_at_6.36.56_PM.0.png"> Batman </a>
-        </li>
-        <li>
-          <a href="/"> Technical </a>
-        </li>
-        <li>
-          <a href="https://i.kym-cdn.com/entries/icons/original/000/048/705/stilgar_dune.jpg"> Lisan Al Gaib </a>
-        </li>
-        <li>
-          <a href="/"> Contact Me </a>
-        </li>
-      </ul>
-    </nav>
-  );
-}
 
 function App() {
   return (
@@ -114,17 +159,11 @@ function App() {
       </>
       <>
         <AboutMe />
-        <div className="contactsss">
-            {contacts.map((element) => (
-              <MyContacts title={element.title} content={element.content} link={element.link} />
-            ))}
-          </div>
-          
-      </>
-   
+        <Batman />
+        <Technical />
+        <Contact />       
+      </>   
     </>
-
-
   )
 }
 
